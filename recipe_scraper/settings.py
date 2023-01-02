12f47f09904241ee04ed2321436f7288a66267ce
victorkuoz <b08902069@csie.ntu.edu.tw>
2023-01-03 01:22:31 +0800

    remove comment

--- recipe_scraper/settings.py
+++ recipe_scraper/settings.py
@@ -4,3 +4,4 @@ NEWSPIDER_MODULE = 'recipe_scraper.spiders'
 
 ROBOTSTXT_OBEY = False
 USER_AGENT = 'Mozilla/5.0 (X11; CrOS i686 4319.74.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36'
+# LOG_LEVEL = 'INFO'
